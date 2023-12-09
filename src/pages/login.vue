4419d90c170e55679f8cb0af86d5893a2f20ef5a
luqianyu <lqygo1234@qq.com>
2023-12-09 10:42:28 +0800

    feat: 登录页

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="login-page">
-    <form @submit.prevent="handleSubmit">
-      <label>
-        用户名:
-        <input v-model="username" type="text" required>
-      </label>
-      <label>
-        密码:
-        <input v-model="password" type="password" required>
-      </label>
-      <button type="submit">登录</button>
-    </form>
+    <div class="login-con">
+      <div class="banner"></div>
+      <div class="login-from">
+        <div class="title">用户登录</div>
+        <div class="input-con">
+          <el-input v-model="username" type="text" placeholder="请输入用户名">
+            <i slot="prefix" class="el-input__icon el-icon-user"></i>
+          </el-input>
+        </div>
+        <div class="input-con">
+          <el-input v-model="password" type="password" placeholder="请输入密码">
+            <i slot="prefix" class="el-input__icon el-icon-lock"></i>
+          </el-input>
+        </div>
+        <div class="btn-con">
+          <el-button @click="handleSubmit" type="primary">登录</el-button>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -51,5 +60,57 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+
+  /* asset login-background.png */
+  background: url('../assets/login-background.png') no-repeat center center;
+  background-color: #F6F8FA;
 }
+#login-page .login-con {
+  width: 1000px;
+  height: 600px;
+  border-radius: 10px;
+  background: #FFFFFF;
+  box-shadow: 0px 4px 10px 0px rgba(35, 115, 210, 0.1);
+
+  display: flex;
+  position: relative;
+}
+#login-page .login-con .banner {
+  width: 50%;
+  height: 100%;
+  background: url('../assets/login-banner.png') no-repeat center center;
+}
+#login-page .login-con .login-from {
+  width: 50%;
+  height: 100%;
+  padding: 0 100px;
+  box-sizing: border-box;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+}
+#login-page .login-con .login-from .title {
+  font-size: 24px;
+  color: #333333;
+  font-weight: bold;
+  margin-bottom: 40px;
+  padding-top: 70px;
+}
+#login-page .login-con .login-from .input-con {
+  margin-bottom: 20px;
+}
+#login-page .login-con .login-from .input-con {
+  width: 100%;
+}
+#login-page .login-con .login-from .btn-con {
+  margin-top: 40px;
+  width: 100%;
+}
+#login-page .login-con .login-from .btn-con .el-button {
+  margin-top: 100px;
+  width: 100%;
+}
+
 </style>
